9793ae3bc74103b11774eed9ad255cff00f68294
Cem <122365538+fcemyuksel@users.noreply.github.com>
2023-10-14 11:29:31 +0300

    depends on method

--- src/test/java/day09_TestNG/C02_DependsOnMethods.java
+++ src/test/java/day09_TestNG/C02_DependsOnMethods.java
@@ -9,45 +9,59 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class C02_DependsOnMethods extends TestBase_BeforeClass {
-    @Test(priority = 1)
-    public void amazonTest() {
-// amazon anasayfaya gidin
+     /*
+        DependsOnMethods priority'den farklidir
+        DependsOnMethods hangi method'un once calisacagina karar vermez
+        Sadece yazildigi test calismadan once, baglandigi testin
+        calistigindan ve PASSED oldugundan emin olmak ister.
+        bagli oldugu test calismaz veya calisir ama PASSED olmazsa
+        DependsOnMethods'un yazildigi test IGNORE olur
+        DependsOnMethods yazildigi method tek basina calistirilmak istenirse
+        once bagli oldugu testin calismasi ve Passed olmasi gerektiginden
+        kendinden once bagli oldugu method'un calismasini saglar
+        Ama bu baglanti birden fazla method'u calistirmaz.
+        priority kullanilarak bir method'un once calismasi istense de
+        o method onceligi bagli oldugu method'a devreder
+        bagli olunan method da baska method'a bagliysa once onu calistirir.
+     */
+
+    @Test
+    public void amazonTest(){
+        // amazon anasayfaya gidin
         driver.get("https://www.amazon.com");
         // url'in amazon icerdigini test edin ve screenshot alin
-        String expectedUrlIcerik="amazon";
-        String actualUrl=driver.getCurrentUrl();
+        String expectedUrlIcerik = "amazon";
+        String actualUrl = driver.getCurrentUrl();
         Assert.assertTrue(actualUrl.contains(expectedUrlIcerik));
-
     }
 
     @Test(dependsOnMethods = "amazonTest")
-    public void NutellaTest() {
-        // Nutella icin arama yapin
-        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
-        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
-        // ilk urune tiklayin
-        driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-square-aspect'])[1]")).click();
-        // urun isminin Nutella icerdigini test edin ve screenshot alin
-        WebElement sonucYaziElementi=driver.findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"));
-        String expectedIcerik="Nutella";
-        String actualSonucYazisi=sonucYaziElementi.getText();
-        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
+    public void nutellaTest(){
 
+        // Nutella icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+        WebElement sonucYaziElementi = driver.findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"));
+
+        String expectedIcerik = "Nutella";
+        String actualSonucYazisi = sonucYaziElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
     }
 
     @Test(dependsOnMethods = "nutellaTest")
-    public void ilkUrunTest() {
+    public void ilkUrunTest(){
+        // ilk urune tiklayin
         driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-square-aspect'])[1]"))
                 .click();
-// urun isminin Nutella icerdigini test edin ve screenshot alin
+        // urun isminin Nutella icerdigini test edin ve urun isminin screenshot alin
 
-        WebElement urunIsmiElementi= driver.findElement(By.xpath("//h1[@id='title']"));
+        WebElement urunIsimElementi= driver.findElement(By.xpath("//h1[@id='title']"));
 
-        String expectedUrunIcerik="Nutella";
-        String actualUrunIsmi=urunIsmiElementi.getText();
+        String expectedUrunIcerik = "Nutella";
+        String actualUrunIsmi = urunIsimElementi.getText();
 
         Assert.assertTrue(actualUrunIsmi.contains(expectedUrunIcerik));
-
-
     }
+
+
 }
