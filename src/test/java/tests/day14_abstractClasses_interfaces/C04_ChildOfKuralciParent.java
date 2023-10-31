739baf82737f5e142778801eb317bbf1eb9ed4e5
Cem Yüksel <fcemyuksel@gmail.com>
2023-10-31 13:19:32 +0300

    child of ruller parent class

---
+++ src/test/java/tests/day14_abstractClasses_interfaces/C04_ChildOfKuralciParent.java
@@ -0,0 +1,23 @@
+package tests.day14_abstractClasses_interfaces;
+
+public class C04_ChildOfKuralciParent extends C03_KuralciParent{
+
+
+    @Override
+    public void method2() {
+        // abstract parent class'lar
+        // child class'lari abstract olarak isaretlenen method'lari
+        // implement etmeye zorlarlar.
+        // method body'sine yani method'u nasil kullanacaklarina karismazlar
+    }
+    public void method1(){
+        // child class'lar parent class'daki concrete method'lari
+        // (abstract olmayan)
+        // kendilerine ISTERLERSE uyarlayabilirler
+
+        // parent class'lardaki concrete method'lari
+        // Override etmek OPSIYONEL'dir
+        // child class isterse override eder
+        // isterse parent class'dan kullanir
+    }
+}
