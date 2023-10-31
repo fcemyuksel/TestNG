8b80b9b9d79d2841937b296da44b4c731f1ced38
Cem Yüksel <fcemyuksel@gmail.com>
2023-10-31 12:29:14 +0300

    abstract classes

---
+++ src/test/java/tests/day14_abstractClasses_interfaces/C01_ClassicParent.java
@@ -0,0 +1,14 @@
+package tests.day14_abstractClasses_interfaces;
+
+public class C01_ClassicParent {
+
+    public void method1(){
+
+    }
+    public void metho2(){
+
+    }
+    public void method3(){
+
+    }
+}
---
+++ src/test/java/tests/day14_abstractClasses_interfaces/C02_ChilfOfClassicParent.java
@@ -0,0 +1,19 @@
+package tests.day14_abstractClasses_interfaces;
+
+public class C02_ChilfOfClassicParent extends C01_ClassicParent{
+
+ /*
+        inheritance yontemleri ile bir class
+        baska bir class'i parent edindiginde
+        butun inisiyatif child class'dadir
+
+         - isterse parent class'daki method'lari aynen kullanabilir
+         - isterse override ederek kendine uyarlar
+         - isterse parent class'da olmayan yeni method'lar olusturabilir
+
+        Parent class'in child class'lar uzerinde hic bir yetkisi yoktur.
+
+
+     */
+
+}
