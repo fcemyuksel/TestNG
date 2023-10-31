0c54b336d6bb6a6b6d6a53a3ffcafa53f758b8f7
Cem Yüksel <fcemyuksel@gmail.com>
2023-10-31 14:29:13 +0300

    abstract parent class

---
+++ src/test/java/tests/day14_abstractClasses_interfaces/C05_AbstractGrandParent.java
@@ -0,0 +1,9 @@
+package tests.day14_abstractClasses_interfaces;
+
+public abstract class C05_AbstractGrandParent {
+
+    public abstract void method1();
+    public abstract void method3();
+
+    public abstract void method4();
+}
