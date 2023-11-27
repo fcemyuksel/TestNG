e1f50e4d6f9134268f1023cb28c82a1f95421e0b
fcemyuksel <fcemyuksel@gmail.com>
2023-11-27 17:16:47 +0300

    Page Object Model driver case

--- src/test/java/Utilies/Driver.java
+++ src/test/java/Utilies/Driver.java
@@ -3,6 +3,9 @@ package Utilies;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -11,22 +14,46 @@ public class Driver {
 
     public static WebDriver getDriver(){
 
+        String browser=ConfigReader.getProperty("browser");
         WebDriverManager.chromedriver().setup();
 
+
         if (driver==null) {
-            driver = new ChromeDriver();
-        }else {
-            //hicbir sey yapma
+            switch (browser){
+                case "safari":
+                    driver=new SafariDriver();
+                    break;
+                case "firefox":
+                    driver=new FirefoxDriver();
+                    break;
+                case "edge":
+                    driver=new EdgeDriver();
+                    break;
+                default:
+                    driver=new ChromeDriver();
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        }    //driver null degilse hicbir sey yapma
             //var olan driver ile devam etme
-        }
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
 
         return driver;
     }
 
     public static void closeDriver(){
-        driver.close();
-        driver=null;
+        if (driver !=null){
+            driver.close();
+            driver=null;
+        }
+    }
+
+    public static void quitDriver(){
+        if (driver !=null){
+            driver.quit();
+            driver=null;
+        }
     }
 }
