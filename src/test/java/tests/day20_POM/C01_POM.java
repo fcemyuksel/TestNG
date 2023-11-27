a7cc3f48b967d156759201264d3f83fdc3a8b4a5
fcemyuksel <fcemyuksel@gmail.com>
2023-11-27 17:39:31 +0300

    Page Object Model test case

---
+++ src/test/java/tests/day20_POM/C01_POM.java
@@ -0,0 +1,31 @@
+package tests.day20_POM;
+
+import Pages.TestOtomasyonuPage;
+import Utilies.ConfigReader;
+import Utilies.Driver;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class C01_POM {
+
+    @Test
+    public void test01(){
+
+        // driver class'indaki Driver methodu private oldugu icin
+        // bunu kullanamayiz ==>  Driver driver=new Driver();
+
+        // testotomasyonu ana sayfaya gidin
+
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        //dress icin arama yapin
+        TestOtomasyonuPage testOtomasyonuPage=new TestOtomasyonuPage();
+        testOtomasyonuPage.aramaKutusu.sendKeys("dress"+ Keys.ENTER);
+
+        // arama sonucunda urun bulunabildigini test edin
+        int bulunanUrunSayisi=testOtomasyonuPage.bulunanUrunlerListesi.size();
+        Assert.assertTrue(bulunanUrunSayisi>0);
+        // sayfayi kapatin
+        Driver.closeDriver();
+    }
+}
