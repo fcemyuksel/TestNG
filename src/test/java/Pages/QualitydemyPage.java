3817a1ee388d933e23b62f202371a475167d8427
fcemyuksel <fcemyuksel@gmail.com>
2023-12-02 16:37:32 +0300

    Pages

--- src/test/java/Pages/QualitydemyPage.java
+++ src/test/java/Pages/QualitydemyPage.java
@@ -1,6 +1,6 @@
 package Pages;
 
-import Utilies.Driver;
+import utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
